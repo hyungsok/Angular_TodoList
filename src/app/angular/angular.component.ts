f9852d83eff28cf8adb2fadc9ad940c4c035b684
kevin <kevin.lee@mangoplate.com>
2018-04-28 11:06:02 +0900

    간단한 애니메이션 페이지 fade in 애니메이션 처리 추가

--- src/app/angular/angular.component.ts
+++ src/app/angular/angular.component.ts
@@ -1,12 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../user.service';
 import {TodoVO} from '../domain/todo.vo';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 
 @Component({
   selector: 'app-augular',
   templateUrl: './angular.component.html',
-  styleUrls: ['./angular.component.scss']
+  styleUrls: ['./angular.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({opacity: 1, transform: 'translate(0, 0)'})),
+      transition('void => in', [
+        style({opacity: 0, transform: 'translate(-100%, 0)'}),
+        animate(1000)])
+    ])
+  ]
 })
 
 export class AngularComponent implements OnInit {

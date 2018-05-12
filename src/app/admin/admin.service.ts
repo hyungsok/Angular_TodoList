3c6655b33c43a82ecb7337c6a5c7df5cd9a954f9
kevin <kevin.lee@mangoplate.com>
2018-05-12 12:08:10 +0900

    관리자사이트: 뉴스 상세보기 서비스 구현
    params 데이터타입의 Observable객체에 subscribe해서 변경된 데이터를 가져온다.

--- src/app/admin/admin.service.ts
+++ src/app/admin/admin.service.ts
@@ -20,4 +20,7 @@ export class AdminService {
     return this.http.post<ResultVO>(this.SERVER + '/api/newsList', params, {headers: this.headers});
   }
 
+  findOneNews(news_id: number) {
+    return this.http.get(this.SERVER + `/api/news?news_id=${news_id}`);
+  }
 }
--- src/app/admin/news/view/view.component.ts
+++ src/app/admin/news/view/view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {AdminService} from '../../admin.service';
 
 @Component({
   selector: 'app-view',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private adminService: AdminService) {
+    console.log(location.pathname);
+    // view 객체를 호출할때 첫번째만 생성, 두번째부터는 안찍힌다. 한번만 생성하므로 Rx Observale패턴으로 URL 변경에 대한 데이터처리를 해줌
+    this.route.params.subscribe(params => {
+      const news_id = +params['news_id']; // +기호는 스트링을 숫자로 변환해줌
+      console.log('news_id : ' + news_id);
+      this.findOneNews(news_id);
+    });
+  }
 
   ngOnInit() {
   }
 
+  findOneNews(news_id: number) {
+    this.adminService.findOneNews(news_id)
+      .subscribe(res => {
+        console.log(res);
+      });
+  }
 }
